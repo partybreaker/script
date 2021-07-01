ac9ffcf895803c0fd925e15663cb4149479a9e34
Tio Kirill <tio.kirill@gmail.com>
2021-07-01 13:19:50 +0300

    v2

---
+++ go.php
@@ -0,0 +1,12 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Frameset//EN" "http://www.w3.org/TR/html4/frameset.dtd">
+<html><head><title>Add2it Go-To System</title></head>
+<FRAMESET rows="*,25" frameborder="NO" border="0" framespacing="0">
+
+		<FRAME src="https://viralplr.com/success-t-404" name="Main" noresize>
+		<FRAME src="to.pl?link=https://viralplr.com/success-t-404&language=eng&user=&f=1&iht=0" name="To" scrolling="no" noresize>
+	</FRAMESET>
+	<NOFRAMES><BODY><CENTER><A href="https://viralplr.com/success-t-404"><font face="Arial, Helvetica, sans-serif" size="+1">Click Here To Continue</font></a>
+
+
+</CENTER></BODY></NOFRAMES>
+</HTML>
